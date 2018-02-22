5ba3cfb00e281fa260804cbf1d84c59233c7aa6d
Corentin Vanson <corentin.vanson@isen-lille.fr>
2018-02-22 23:34:06 +0100

    3rd

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,11 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  port: 80,
+  proxy: {
+    "/api/*": {
+      target: "http://localhost:3000",
+      secure: false       
+    }
+  }
 }
